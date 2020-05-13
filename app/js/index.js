c1c5406444b5dabd98a0927d12c08709cd0c0462
Biloholovskyi <biloholovskyi>
2020-05-13 15:44:13 +0300

    blog

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,12 @@
 import 'normalize.css';
 import {startFullPage} from "./fullPage";
 import {shapeText} from "./shapeAnimations";
-import {heightPoster} from "./collections";
 
 $(document).ready(() => {
   startFullPage();
   shapeText();
 
   // collections slider
-  heightPoster();
-
   $('.collections__slider').owlCarousel({
     margin: 16,
     autoWidth: true,
