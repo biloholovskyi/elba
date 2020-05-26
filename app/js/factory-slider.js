f6b31922cb188ba05a4bbd205c7552db8680e188
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-05-26 18:07:24 +0300

    responsive filter/single-product/search

--- app/js/factory-slider.js
+++ app/js/factory-slider.js
@@ -12,5 +12,11 @@ $(document).ready(function(){
         nav: true,
         items: 1
       });
+      $('#single-owl').owlCarousel({
+        margin: 10,
+        loop: true,
+        nav: false,
+        items: 1
+      });
 
 });
